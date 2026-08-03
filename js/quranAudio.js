fcae2007284daa21d21d0329d4360c683bf7efd2
Sayyaf <athmanbilal16@gmail.com>
2026-08-04 02:30:48 +0300

    Update quranAudio.js

--- js/quranAudio.js
+++ js/quranAudio.js
@@ -1,65 +1,27 @@
 const API = "https://api.quran.com/api/v4";
 
-// Load all reciters
-async function getReciters() {
+// Get all reciters
+async function getReciters(){
 
     const response = await fetch(
-        `${API}/resources/recitations`
+        API + "/resources/recitations"
     );
 
-    const data = await response.json();
+    const json = await response.json();
 
-    return data.recitations;
+    return json.recitations;
 
 }
 
-// Get audio for one Surah
-async function getSurahAudio(reciterId, surahNumber) {
+// Get Surah audio
+async function getSurahAudio(reciter, surah){
 
     const response = await fetch(
-        `${API}/chapter_recitations/${reciterId}/${surahNumber}`
+        API + "/chapter_recitations/" + reciter + "/" + surah
     );
 
-    const data = await response.json();
+    const json = await response.json();
 
-    return data.audio_file;
-
-}
-async function loadReciterList() {
-
-    const reciters = await getReciters();
-
-    const select = document.getElementById("reciter");
-
-    select.innerHTML = "";
-
-    reciters.forEach(reciter => {
-
-        const option = document.createElement("option");
-
-        option.value = reciter.id;
-        option.textContent = reciter.reciter_name;
-
-        select.appendChild(option);
-
-    });
-
-    const saved = localStorage.getItem("reciterId");
-
-    if(saved){
-        select.value = saved;
-    }
-
-}
-
-async function playCurrentSurah(surahNumber){
-
-    const reciterId = document.getElementById("reciter").value;
-
-    localStorage.setItem("reciterId", reciterId);
-
-    const audio = await getSurahAudio(reciterId, surahNumber);
-
-    document.getElementById("player").src = audio.audio_url;
+    return json.audio_file;
 
 }
