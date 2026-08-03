db952e2d8915498ec4966a5a9113720ffbd9177d
Sayyaf <athmanbilal16@gmail.com>
2026-08-04 00:59:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -116,3 +116,10 @@ if(navigator.geolocation){
 const script = document.createElement("script");
 script.src = "notification.js";
 document.body.appendChild(script);
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("service-worker.js")
+            .then(() => console.log("Service Worker Registered"))
+            .catch(err => console.log(err));
+    });
+}
