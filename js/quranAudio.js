6922057f2f75ebfa748456350857284cabbeeee4
Sayyaf <athmanbilal16@gmail.com>
2026-08-04 02:25:50 +0300

    Update quranAudio.js

--- js/quranAudio.js
+++ js/quranAudio.js
@@ -1,1 +1,27 @@
+const API = "https://api.quran.com/api/v4";
 
+// Load all reciters
+async function getReciters() {
+
+    const response = await fetch(
+        `${API}/resources/recitations`
+    );
+
+    const data = await response.json();
+
+    return data.recitations;
+
+}
+
+// Get audio for one Surah
+async function getSurahAudio(reciterId, surahNumber) {
+
+    const response = await fetch(
+        `${API}/chapter_recitations/${reciterId}/${surahNumber}`
+    );
+
+    const data = await response.json();
+
+    return data.audio_file;
+
+}
