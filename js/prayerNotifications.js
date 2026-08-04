5c439ed9474142101378e5c52da04ffbd7a12523
Sayyaf <athmanbilal16@gmail.com>
2026-08-04 15:26:11 +0300

    Update prayerNotifications.js

--- js/prayerNotifications.js
+++ js/prayerNotifications.js
@@ -1,1 +1,17 @@
+function playAdhan(type = "normal") {
 
+    const audio = new Audio();
+
+    if(type === "fajr"){
+
+        audio.src = "../audio/fajr.mp3";
+
+    }else{
+
+        audio.src = "../audio/adhan.mp3";
+
+    }
+
+    audio.play();
+
+}
